215813920b5490d379e4861016327c9ea59411b3
Thiago Tadelle <thiago.tadelle@gmail.com>
2021-06-21 14:24:54 -0300

    Primeira versão do frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt-br'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
